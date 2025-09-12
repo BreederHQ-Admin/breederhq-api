8129d21a6002f0eda121ec437a6004adcb860a25
Aaron Payne <aaronpayneconsulting@gmail.com>
2025-09-12 10:10:39 -0500

    changed prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 // prisma.config.ts
+import "dotenv/config";              // loads .env by default; can be overridden with DOTENV_CONFIG_PATH
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  // Point Prisma CLI to your schema file
-  schema: "prisma/schema.prisma"
+  schema: "prisma/schema.prisma",
 });
